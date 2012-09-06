8215236bb3468f47f82c40f9aa18b6745bd6a112
Sandro Turriate <sandro.turriate@gmail.com>
2012-09-06 01:04:58 -0700

    Add visor

--- app.rb
+++ app.rb
@@ -1,43 +1,20 @@
 require 'bundler'
 Bundler.require
 autoload :Base64, 'base64'
+autoload :JSON, 'json'
 
 module StarfleetMe
+  Infinity = 1.0/0
 
-  class Generator
-    Infinity = 1.0/0
-
-    SHIPS = {
-      (0..100) => './public/uss_tiny.png',
-      (101..300) =>  './public/uss_small.png',
-      (301..499) => './public/uss_medium.png',
-      (500..Infinity) => './public/uss_large.png'
-    }
-
-    FRAMES = 8
-
+  class Base
     attr_reader :source
 
     def initialize(image_source=url)
       @source = CGI.unescape image_source.to_s
     end
 
-    def animate
-      steps = []
-      x = 0
-      y = height/6
-      xstep = (width/FRAMES).ceil
-      ystep = (y/FRAMES + 1).ceil
-      (FRAMES).times do |index|
-        if x <= width && y >= 0
-          steps << "-page +#{x}+#{y} #{ship}"
-        end
-        x += xstep
-        y -= ystep
-      end
-      cmd = "convert -delay 14 -loop 100 -dispose none #{input_path} -dispose previous #{steps.join(" ")} #{output_path}"
-      puts cmd
-      system cmd
+    def convert
+      raise 'not implemented'
     end
 
     def digest
@@ -56,12 +33,6 @@ module StarfleetMe
       @size ||= `identify -format "%G" #{input_path}`.split /[x\r?\n]/
     end
 
-    def ship
-      @ship ||= SHIPS.each do |range, name|
-        break name if range.include? width
-      end
-    end
-
     def width
       size[0].to_i
     end
@@ -71,14 +42,103 @@ module StarfleetMe
     end
 
     def output_path
-      @output_path ||= File.expand_path("./tmp/#{digest}.gif")
+      @output_path ||= File.expand_path("./tmp/#{digest}.jpg")
     end
 
     def valid?
       source && !source.empty? && download
     end
+  end
 
-    # convert -delay 50 -dispose none sandrot.jpg -dispose previous -page +0+150 small_ship.png -page +150+100 small_ship.png -page +300+50 small_ship.png -page +450+0 small_ship.png -loop 0 anim.gif
+  class VisorGenerator < Base
+
+    def convert
+      visor_path = File.expand_path("./public/visor.png")
+      cmd = "convert #{input_path} #{visor_path} -geometry #{visor_width}x#{visor_position} -composite #{output_path}"
+      puts cmd
+      system cmd
+    end
+
+    def detection
+      endpoint = "https://lambda-face-detection-and-recognition.p.mashape.com/detect"
+      @detection ||= `curl -s -H "X-Mashape-Authorization: cXVremxybWhidWUzM3k1YTdkZnp4N3M4MHQwZWxiOmRlYTcyMmIzMGE0YzQzMjA3M2Q1MGIzNWFhZmNiMzIyODBiM2JkYWI=" "#{endpoint}?images=#{source}"`
+    end
+
+    def detection_json
+      JSON.parse(detection)
+    end
+
+    def face_tags
+      tags = detection_json.fetch('photos', [{}]).first.fetch('tags', [{}]).first
+    end
+
+    def valid?
+      p face_tags
+      super && face_tags && face_tags['eye_left'] && face_tags['eye_right']
+    end
+
+    def eye_width
+      face_tags['eye_left']['x'] - face_tags['eye_right']['x']
+    end
+
+    def visor_width
+      (eye_width * 3).ceil
+    end
+
+    def visor_y
+      y = face_tags['eye_right']['y']
+      y - visor_width * 1/6
+    end
+
+    def visor_x
+      x = face_tags['eye_right']['x']
+      x - eye_width
+    end
+
+    def visor_position
+      "+#{visor_x}+#{visor_y}"
+    end
+
+  end
+
+  class ShipGenerator < Base
+
+    SHIPS = {
+      (0..100) => './public/uss_tiny.png',
+      (101..300) =>  './public/uss_small.png',
+      (301..499) => './public/uss_medium.png',
+      (500..Infinity) => './public/uss_large.png'
+    }
+
+    FRAMES = 8
+
+    def convert
+      steps = []
+      x = 0
+      y = height/6
+      xstep = (width/FRAMES).ceil
+      ystep = (y/FRAMES + 1).ceil
+      (FRAMES).times do |index|
+        if x <= width && y >= 0
+          steps << "-page +#{x}+#{y} #{ship}"
+        end
+        x += xstep
+        y -= ystep
+      end
+      cmd = "convert -delay 14 -loop 100 -dispose none #{input_path} -dispose previous #{steps.join(" ")} #{output_path}"
+      puts cmd
+      system cmd
+    end
+
+    def output_path
+      @output_path ||= File.expand_path("./tmp/#{digest}.gif")
+    end
+
+    def ship
+      @ship ||= SHIPS.each do |range, name|
+        break name if range.include? width
+      end
+    end
 
   end
 end
@@ -107,12 +167,12 @@ DB_GIFS = DB[:gifs]
 
 get '/' do
   if params[:source] && !params[:source].empty?
-    generator = StarfleetMe::Generator.new params[:source]
+    generator = StarfleetMe::ShipGenerator.new params[:source]
     etag generator.digest
     gif = DB_GIFS.where(source: generator.source).first
     if gif.nil?
       if generator.valid?
-        generator.animate
+        generator.convert
         if File.exists? generator.output_path
           binary = File.open(generator.output_path, 'r:binary') {|f| p f.external_encoding.name; f.read }
           binary = Base64.encode64(binary)
@@ -137,9 +197,16 @@ get '/' do
   haml :index
 end
 
-get '/foo' do
-  etag Digest::SHA1.hexdigest('123')
-  p env['HTTP_IF_NONE_MATCH']
-  p response['ETag']
-  Time.now.to_s
+get '/visor' do
+  if params[:source] && !params[:source].empty?
+    generator = StarfleetMe::VisorGenerator.new params[:source]
+    # etag generator.digest
+    if generator.valid?
+      generator.convert
+      send_file generator.output_path, :type => :gif
+    end
+  end
+
+  status 502
+  "Face detection failed."
 end
